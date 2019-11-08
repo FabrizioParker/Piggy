97bdc3f78f81c68444ff8346f606eaefcdbf9fa5
Gia Parker <parkerg@gilmour.org>
2019-11-08 09:06:27 -0500

    tab error in left or right

--- student.py
+++ student.py
@@ -122,12 +122,12 @@ class Piggy(PiggyParent):
             else:
                 left_total += dist
                 left_count += 1
-            left_avg = left_total / left_count
-            right_avg = right_total / right_count
-            if left_avg > right_avg:
-                self.turn_by_deg(-45)
-            else:
-                self.turn_by_deg(45)
+        left_avg = left_total / left_count
+        right_avg = right_total / right_count
+        if left_avg > right_avg:
+            self.turn_by_deg(-45)
+        else:
+            self.turn_by_deg(45)
                 
     def twist(self):
         """turns right then left"""
