db447be38579b6da640feb296b6f49737bd70c65
Gia Parker <parkerg@gilmour.org>
2019-11-15 13:31:02 -0500

    change max corner count to 3

--- student.py
+++ student.py
@@ -126,7 +126,7 @@ class Piggy(PiggyParent):
         """turn left or right depending on averaged scan"""
         #traversal
         self.corner_count += 1
-        if self.corner_count > 5:
+        if self.corner_count > 3:
             self.turn_by_deg(180)
         left_total = 0
         left_count = 0
